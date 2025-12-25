2c8835228419479ff1c0af12b5a554d32ce86730
moyuyanli <572490972@qq.com>
2025-12-26 01:13:45 +0800

    refactor(DriveType): 重构数据库驱动类型枚举结构
    - 将 DriveType 枚举的属性移除，改为扩展属性实现
    - 添加 urlPrefix 扩展属性用于获取 JDBC URL 前缀
    - 添加 driverClass 扩展属性用于获取驱动程序类名
    - 保持原有的数据库类型枚举值不变
    - 通过 when 表达式实现属性值的映射逻辑
    - 提高代码可读性和维护性

--- src/main/kotlin/cn/chahuyun/hibernateplus/DriveType.kt
+++ src/main/kotlin/cn/chahuyun/hibernateplus/DriveType.kt
@@ -6,34 +6,64 @@ package cn.chahuyun.hibernateplus
  * @author Moyuyanli
  * @date 2024/7/18 14:12
  */
-enum class DriveType(val urlPrefix: String, val driverClass: String) {
+enum class DriveType {
     /**
      * H2类型数据库
      */
-    H2("jdbc:h2:file:", "org.h2.Driver"),
+    H2,
 
     /**
      * sqlite类型数据库
      */
-    SQLITE("jdbc:sqlite:", "org.sqlite.JDBC"),
+    SQLITE,
 
     /**
      * HSQLDB 类型数据库 (Java 界的 SQLite，更强的并发支持)
      */
-    HSQLDB("jdbc:hsqldb:file:", "org.hsqldb.jdbc.JDBCDriver"),
+    HSQLDB,
 
     /**
      * mysql类型数据库
      */
-    MYSQL("jdbc:mysql://", "com.mysql.cj.jdbc.Driver"),
+    MYSQL,
 
     /**
      * MariaDB类型数据库
      */
-    MARIADB("jdbc:mariadb://", "org.mariadb.jdbc.Driver"),
+    MARIADB,
 
     /**
      * DuckDB类型数据库
      */
-    DUCKDB("jdbc:duckdb:", "org.duckdb.DuckDBDriver");
+    DUCKDB
 }
+
+/**
+ * 获取数据库驱动类型的JDBC URL前缀
+ *
+ * @return 对应数据库类型的JDBC URL前缀字符串
+ */
+val DriveType.urlPrefix: String
+    get() = when (this) {
+        DriveType.H2 -> "jdbc:h2:file:"
+        DriveType.SQLITE -> "jdbc:sqlite:"
+        DriveType.HSQLDB -> "jdbc:hsqldb:file:"
+        DriveType.MYSQL -> "jdbc:mysql://"
+        DriveType.MARIADB -> "jdbc:mariadb://"
+        DriveType.DUCKDB -> "jdbc:duckdb:"
+    }
+
+/**
+ * 获取数据库驱动类型的驱动程序类名
+ *
+ * @return 对应数据库类型的JDBC驱动程序完全限定类名
+ */
+val DriveType.driverClass: String
+    get() = when (this) {
+        DriveType.H2 -> "org.h2.Driver"
+        DriveType.SQLITE -> "org.sqlite.JDBC"
+        DriveType.HSQLDB -> "org.hsqldb.jdbc.JDBCDriver"
+        DriveType.MYSQL -> "com.mysql.cj.jdbc.Driver"
+        DriveType.MARIADB -> "org.mariadb.jdbc.Driver"
+        DriveType.DUCKDB -> "org.duckdb.DuckDBDriver"
+    }
